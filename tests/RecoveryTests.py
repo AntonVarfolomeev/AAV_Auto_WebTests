b7b447d064445320e15233acd03dadb9132f975d
antonvarfolomeev <varfolomeev2202@gmail.com>
2025-10-23 21:43:21 +0300

    Added Recovery Page and Recovery tests with "for" cycle

---
+++ tests/RecoveryTests.py
@@ -0,0 +1,25 @@
+import allure
+
+from core.BaseTest import browser
+from pages.BasePage import BasePage
+from pages.LoginPage import LoginPageHelper
+from pages.RecoveryPage import RecoveryPageHelper
+from conftest import username
+
+BASE_URL = 'https://ok.ru/'
+PASSWORD_TEXT = '1'
+
+@allure.suite('Проверка восстановления данных пользователя')
+@allure.title('Проверка перехода к восстановлению после нескольких неудачных попыток авторизации')
+def test_go_to_recovery_after_multiple_fails(browser, username):
+    BasePage(browser).get_url(BASE_URL)
+    LoginPage = LoginPageHelper(browser)
+    LoginPage.type_login(username)
+
+    for i in range (3):
+        LoginPage.type_password(PASSWORD_TEXT)
+        LoginPage.click_login()
+
+    LoginPage.click_recovery()
+    RecoveryPageHelper(browser)
+
